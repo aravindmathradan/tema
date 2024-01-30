f652df6f1d9012c8348fb77521fff8d65698c720
Aravind <aravindmathradan@gmail.com>
2024-01-30 18:14:52 +0530

    implemented authorization middleware to restrict unauthorized access

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
@@ -14,16 +15,18 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/projects", app.listProjectsHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/projects", app.createProjectHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/projects/:id", app.viewProjectHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/projects/:id", app.updateProjectHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/projects/:id", app.deleteProjectHandler)
-
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activate", app.activateUserHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+	protected := alice.New(app.requireActivatedUser)
+	router.Handler(http.MethodGet, "/v1/projects", protected.ThenFunc(app.listProjectsHandler))
+	router.Handler(http.MethodPost, "/v1/projects", protected.ThenFunc(app.createProjectHandler))
+	router.Handler(http.MethodGet, "/v1/projects/:id", protected.ThenFunc(app.viewProjectHandler))
+	router.Handler(http.MethodPatch, "/v1/projects/:id", protected.ThenFunc(app.updateProjectHandler))
+	router.Handler(http.MethodDelete, "/v1/projects/:id", protected.ThenFunc(app.deleteProjectHandler))
+
+	standard := alice.New(app.recoverPanic, app.rateLimit, app.authenticate)
+	return standard.Then(router)
 }
