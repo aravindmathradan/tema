9de70e9f47d2f1f585a916fbc9d4d1a81c7e1c82
Aravind <aravindmathradan@gmail.com>
2024-02-08 07:53:13 +0530

    ui: signup form width change in smaller screens

--- frontend/src/routes/(auth)/signup/+layout.svelte
+++ frontend/src/routes/(auth)/signup/+layout.svelte
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center h-screen w-screen border">
-	<div class="flex flex-col md:flex-row md:gap-10 md:w-8/12">
+	<div class="flex flex-col md:flex-row md:gap-10 w-4/5 md:w-8/12">
 		<slot />
 	</div>
 </div>
