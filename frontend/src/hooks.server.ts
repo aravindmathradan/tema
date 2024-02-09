004e3594ed59e5a6d42281de9ac33b0cf0e17669
Aravind <aravindmathradan@gmail.com>
2024-02-09 13:58:53 +0530

    ui: snake_case keys in api response are converted to camelCase keys using handleFetch hook

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,5 +1,6 @@
 import { getAuthenticatedUser } from "$lib/server/auth";
-import { redirect, type Handle } from "@sveltejs/kit";
+import { redirect, type Handle, type HandleFetch, error } from "@sveltejs/kit";
+import camelcaseKeys from "camelcase-keys";
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.user = await getAuthenticatedUser(event);
@@ -13,4 +14,29 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 
 	return response;
+};
+
+export const handleFetch: HandleFetch = async ({ request, fetch }) => {
+	try {
+		const res = await fetch(request);
+		const response = await res.json();
+
+		let camelcaseResponse = camelcaseKeys(response, { deep: true });
+
+		return new Response(JSON.stringify(camelcaseResponse), {
+			headers: res.headers,
+			status: res.status,
+			statusText: res.statusText,
+		});
+	} catch (err: any) {
+		if (err.cause?.code === "ECONNREFUSED" || err.cause?.code === "ECONNTIMEOUT") {
+			error(500, {
+				message: "Could not connect to the server. Please try again later",
+			});
+		} else {
+			error(500, {
+				message: "Something went wrong. Please try again later",
+			});
+		}
+	}
 };
